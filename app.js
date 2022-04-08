b39f5ae2d52512fdcf59dafd57d66dff716a9fab
chinghuihui <sunny70624@gmail.com>
2022-04-08 12:06:57 +0800

    test

--- app.js
+++ app.js
@@ -1,65 +1,103 @@
-const { WebClient } = require("@slack/web-api");
-const { createEventAdapter } = require("@slack/events-api");
+require('dotenv').config()
 
-const slackSigningSecret = process.env.SLACK_SIGNING_SECRET;
-const slackToken = process.env.SLACK_TOKEN;
-const port = process.env.SLACK_PORT || 3000;
+const { WebClient } = require('@slack/web-api')
+const { createEventAdapter } = require('@slack/events-api')
 
-const slackEvents = createEventAdapter(slackSigningSecret);
-const slackClient = new WebClient(slackToken);
+const slackSigningSecret = process.env.SLACK_SIGNING_SECRET
 
-function sendMessage({ channel, text }) {
-  (async () => {
-    try {
-      await slackClient.chat.postMessage({
-        channel,
-        text,
-      });
-    } catch (error) {
-      console.log(error.data);
-    }
-  })();
+const slackToken = process.env.SLACK_TOKEN
+const port = process.env.SLACK_PORT || 3000
+
+const slackEvents = createEventAdapter(slackSigningSecret)
+const slackClient = new WebClient(slackToken)
+
+async function sendMessage({ channel, text }) {
+  try {
+    await slackClient.chat.postMessage({
+      channel,
+      text,
+    })
+  } catch (error) {
+    console.log(error.data)
+  }
 }
 
-slackEvents.on("app_mention", (event) => {
-  const messageFromUser = event.text;
-  const user = event.user;
+slackEvents.on('app_mention', (event) => {
+  const messageFromUser = event.text
+  const user = event.user
 
   // TODO: get the user list
-  if (messageFromUser.includes("給我名單")) {
+  if (messageFromUser.includes('設計部讀書會')) {
     sendMessage({
       channel: event.channel,
-      text: `蛤 給你拉`,
-    });
+      text: `給你設計部讀書會清單`,
+    })
 
-    return;
+    return
   }
 
-  if (!messageFromUser.toLowerCase().includes("參加")) {
-    (async () => {
+  if (messageFromUser.includes('技術部讀書會')) {
+    sendMessage({
+      channel: event.channel,
+      text: `給你技術部讀書會清單`,
+    })
+
+    return
+  }
+
+  if (messageFromUser.includes('PM部讀書會')) {
+    sendMessage({
+      channel: event.channel,
+      text: `給你PM部讀書會清單`,
+    })
+
+    return
+  }
+
+  if (messageFromUser.includes('我要參加')) {
+    ;(async () => {
       const info = await slackClient.users.info({
         user: event.user,
-      });
-      console.log("info", info.user.profile);
-    })();
+      })
+      console.log('info', info.user.profile)
+      const bot = await slackClient.bots.info()
 
+      console.log(
+        await slackClient.conversations.members({ channel: event.channel })
+      )
+    })()
+
+    // TODO: add into the list
     sendMessage({
       channel: event.channel,
-      text: `誒？ <@${user}>! 哩公啥？`,
-    });
+      text: `Hello <@${user}>! YOU ARE IN :tada:`,
+    })
 
-    return;
+    return
   }
 
-  // TODO: add into the list
+  if (messageFromUser.includes('我要退出')) {
+    ;(async () => {
+      console.log('info', info.user.profile)
+    })()
+
+    // TODO: add into the list
+    sendMessage({
+      channel: event.channel,
+      text: `Bye <@${user}>! YOU ARE OUT :wave:`,
+    })
+
+    return
+  }
+
   sendMessage({
     channel: event.channel,
-    text: `Hello <@${user}>! YOU ARE IN :tada:`,
-  });
-});
+    text: `誒？ <@${user}>! 哩公啥？`,
+  })
+})
 
-slackEvents.on("error", console.error);
+slackEvents.on('error', console.error)
 
 slackEvents.start(port).then(() => {
-  console.log(`Server started on port ${port}`);
-});
+  console.log(`Server started on port ${port}`)
+})
